4abc652408e837b2b13f3e7823609ff059e94e11
Akpobome Akpevwe <akpobomeakp@gmail.com>
2020-12-27 00:43:44 +0100

    robohub update

---
+++ src/robots.js
@@ -0,0 +1,61 @@
+export const robots = [{
+        id: 1,
+        name: 'Leanne Graham',
+        username: 'Bret',
+        email: 'Leannegraham@gmail.com'
+    },
+    {
+        id: 2,
+        name: 'Ervin Howell',
+        username: 'Anto',
+        email: 'Ervinhowell@gmail.com'
+    },
+    {
+        id: 3,
+        name: 'Clementine Bauch',
+        username: 'Samantha',
+        email: 'Clementinebauch@gmail.com'
+    },
+    {
+        id: 4,
+        name: 'Patricia Lebsack',
+        username: 'Karianne',
+        email: 'Patricialebsack@gmail.com'
+    },
+    {
+        id: 5,
+        name: 'Chelsey Dietrich',
+        username: 'Ava',
+        email: 'Chelseydietrich@gmail.com'
+    },
+    {
+        id: 6,
+        name: 'Mrs. Dennis Schulist',
+        username: 'Leopoldo_Corkery',
+        email: 'Dennisschulist@gmail.com'
+    },
+    {
+        id: 7,
+        name: 'Bennet Cook',
+        username: 'Volx',
+        email: 'Bennetcook@gmail.com'
+    },
+    {
+        id: 8,
+        name: 'Fenti Asylnia',
+        username: 'Forsley',
+        email: 'Fentiasylnia@gmail.com'
+    },
+    {
+        id: 9,
+        name: 'Marcob Richard',
+        username: 'Basch',
+        email: 'Marcobrichard@gmail.com'
+    },
+    {
+        id: 10,
+        name: 'Timi Lauren',
+        username: 'Rotimi',
+        email: 'Timilauren@gmail.com'
+    }
+];
